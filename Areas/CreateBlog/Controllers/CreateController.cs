cdecca10fd0f8bb240a0da88f78c5cee2044dcf4
leoball <leoball@johnsonfitness.com>
2022-05-10 08:41:21 +0800

    CreateBlog Area建立

---
+++ Areas/CreateBlog/Controllers/CreateController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCHomework6.Areas.CreateBlog.Controllers
+{
+    [Area("CreateBlog")]
+    public class CreateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
