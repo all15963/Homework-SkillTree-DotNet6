5ba04db3c94c919cdcf7865a7ddc35d23edfc0aa
leoball <leoball@johnsonfitness.com>
2022-05-16 13:46:21 +0800

    後端 新增Blog寫入功能

--- Areas/CreateBlog/Controllers/CreateController.cs
+++ Areas/CreateBlog/Controllers/CreateController.cs
@@ -10,11 +10,13 @@ namespace MVCHomework6.Areas.CreateBlog.Controllers
     {
         private readonly ILogger<CreateController> _logger;
         private readonly BlogDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
         
-        public CreateController(ILogger<CreateController> logger, BlogDbContext context)
+        public CreateController(ILogger<CreateController> logger, BlogDbContext context, IWebHostEnvironment environment)
         {
             _logger = logger;
             _context = context;
+            _webHostEnvironment = environment;
         }
 
         public IActionResult Index()
@@ -32,7 +34,45 @@ namespace MVCHomework6.Areas.CreateBlog.Controllers
         [HttpPost]
         public async Task<IActionResult> IndexAsync(CreateBlogViewModel model)
         {
-            return View();
+            var tagCloud = new List<SelectListItem>();
+            foreach (var tag in _context.TagCloud.ToList())
+            {
+                tagCloud.Add(new SelectListItem { Value = tag.Name, Text = tag.Name });
+            }
+            model.TagCloud = tagCloud;
+
+            if (ModelState.IsValid)
+            {
+                string uploads = Path.Combine(_webHostEnvironment.WebRootPath, "uploads");
+                string filePath = string.Empty;
+                var suffix = Path.GetExtension(model.CoverPhoto.FileName);
+                var fileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                if (model.CoverPhoto.Length > 0)
+                {
+                    filePath = Path.Combine(uploads, fileName + suffix);
+                    using (Stream fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await model.CoverPhoto.CopyToAsync(fileStream);
+                        await fileStream.DisposeAsync();
+                    }
+                }
+
+                var article = new Articles
+                {
+                    Id = Guid.NewGuid(),
+                    Title = model.Title,
+                    Body = model.Body,
+                    CreateDate = model.CreateDate,
+                    CoverPhoto = $"uploads\\{fileName + suffix}",
+                    Tags = string.Join(",", model.Tags)
+                };
+                
+                _context.Add(article);
+                await _context.SaveChangesAsync();
+                ViewData["msg"] = "新增成功";
+            }
+
+            return View(model);
         }
     }
 }
